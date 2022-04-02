6e8bb2463fac06d7d76605f9cb319fa217bec54b
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-04-03 02:59:13 +0600

    call dynamic api click with button

--- js/countries.js
+++ js/countries.js
@@ -5,22 +5,41 @@ const loadCountries = () => {
 }
 loadCountries()
 const displayCountires = (countires) => {
-    // console.log(countires)
     const countriesContainer = document.getElementById('countries-container');
-    // console.log(countriesContainer)
+    const spinner = document.getElementById('spinner')
+    spinner.style.display = 'none'
     countires.forEach((countire) => {
-        console.log(countire)
         const div = document.createElement('div')
-        // console.log(div)
         div.classList.add('col-md-4')
         div.innerHTML = `
-            <div class="m-1 p-5 bg-info text-center rounded" style="height: 400px;">
+            <div class="m-1 p-5 bg-info text-center rounded" style="height: 450px;">
                 <h3>Name: ${countire.name}</h3>
-                <h3>Name: ${countire.capital}</h3>
+                <h3>Capital: ${countire.capital}</h3>
                 <img class="img-fluid rounded mt-4" src="${countire.flag}" />
-
+                <button onclick="loadCountireDitels('${countire.name}')" class="btn btn-success mt-2 p-2">See Details</button
             </div>
         `
         countriesContainer.appendChild(div)
     })
+}
+
+const loadCountireDitels = (name) => {
+    const url = `https://restcountries.com/v2/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => dispalyCountrieDetails(data[0]))
+}
+
+const dispalyCountrieDetails = countire => {
+    console.log(countire)
+    const countriDitels = document.getElementById('countrie-ditels')
+    countriDitels.textContent = '';
+    const div = document.createElement('div')
+    div.innerHTML = `
+        <h2>Countrie Diteils</h2>
+        <h3>Name: ${countire.name}</h3> 
+        <h4>Region: ${countire.region}</h4> 
+        <img class="img-fluid rounded mt-4" src="${countire.flag}"/>
+    `
+    countriDitels.appendChild(div)
 }
